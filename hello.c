d26c5d9f4e6f2dfdb7290426a516db9d17ba17ca
Athul Suresh <athulsure@gmail.com>
2017-10-11 22:32:17 +0530

    Add hello

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include <iostream.h>
+
+void output();
+
+int main() 
+{
+        int i = 0;
+        i++;
+        j++;
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        input_function();
+        printf("Hello world: %d", i);
+}
